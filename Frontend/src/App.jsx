7c2fa2fcd5b456729b01103a70d3ae7a264ef674
Dharun Nagavel <dharunnagavel1226@gmail.com>
2026-02-02 18:53:27 +0530

    tailwindcss fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,13 +1,6 @@
-// Import Firebase core
 import { initializeApp } from "firebase/app";
-
-// Import Firebase Auth
 import { getAuth } from "firebase/auth";
-
-// (Optional) Analytics
 import { getAnalytics } from "firebase/analytics";
-
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAvUkZQWVwzY-gijoKUxNYj5BPfRxi4aLE",
   authDomain: "lost-and-locate-fe893.firebaseapp.com",
@@ -17,12 +10,6 @@ const firebaseConfig = {
   appId: "1:260208580833:web:b28ac95906fede20c1ffa0",
   measurementId: "G-DVD9JW350W",
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// ✅ EXPORT AUTH (THIS FIXES YOUR ERROR)
 export const auth = getAuth(app);
-
-// Optional: Analytics
 export const analytics = getAnalytics(app);
